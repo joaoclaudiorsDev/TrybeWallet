671997da11184a7e7d537132769e1e7fdfadfc66
João Cláudio <joaoclaudiobio@gmail.com>
2023-08-07 11:01:04 -0300

    req 5 completado/ falta tipar

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,5 +1,60 @@
-function Table() {
-  return <div>Table</div>;
+import React from 'react';
+import mockData from '../tests/helpers/mockData';
+import { ExpenseData } from '../types';
+
+function getCurrencyFullName(currencyCode) {
+  const currency = mockData[currencyCode];
+  if (currency) {
+    return `${currency.name} (${currency.code}/${currency.codein})`;
+  }
+  return currencyCode;
+}
+
+function Table({ expenses }) {
+  const convertValue = (value, exchangeRate) => {
+    return (value * exchangeRate).toFixed(2);
+  };
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Descrição</th>
+          <th>Tag</th>
+          <th>Método de pagamento</th>
+          <th>Valor</th>
+          <th>Moeda</th>
+          <th>Câmbio utilizado</th>
+          <th>Valor convertido</th>
+          <th>Moeda de conversão</th>
+          <th>Editar/Excluir</th>
+        </tr>
+      </thead>
+      <tbody>
+        {expenses.map((expense) => {
+          const currency = mockData[expense.currency];
+          const exchangeRate = currency ? parseFloat(currency.ask) : 1;
+          const convertedValue = convertValue(parseFloat(expense.value), exchangeRate);
+
+          return (
+            <tr key={ expense.id }>
+              <td>{expense.description}</td>
+              <td>{expense.tag}</td>
+              <td>{expense.method}</td>
+              <td>{parseFloat(expense.value).toFixed(2)}</td>
+              <td>{getCurrencyFullName(expense.currency)}</td>
+              <td>{exchangeRate.toFixed(2)}</td>
+              <td>{convertedValue}</td>
+              <td>Real</td>
+              <td>
+                <button> Botão bem massa</button>
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 }
 
 export default Table;
--- src/pages/Wallet.tsx
+++ src/pages/Wallet.tsx
@@ -1,11 +1,15 @@
+import { useSelector } from 'react-redux';
 import Header from '../components/Header';
 import WalletForm from '../components/WalletForm';
+import Table from '../components/Table';
 
 function Wallet() {
+  const expenses = useSelector((state) => state.wallet.expenses);
   return (
     <>
       <Header />
       <WalletForm />
+      <Table expenses={ expenses } />
     </>
   );
 }
